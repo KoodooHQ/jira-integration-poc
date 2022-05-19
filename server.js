abeeb595f981d2fdef566f7bc16923e622c5894a
Bharat <bharat@BC-MAC-M01.local>
2022-05-19 15:56:15 +0530

    add enpoint to change the status of jira ticket

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 require('dotenv').config();
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const port = 3000
 
 const axios = require('axios');
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+const jiraRequestHeaders = {
+    Authorization: `Basic ${Buffer.from(`${process.env.JIRA_EMAIL}:${process.env.JIRA_API_TOKEN}`).toString("base64")}`
+};
+
+const siteUrl = `https://${process.env.DOMAIN}.atlassian.net`
+
 app.get('/case/:caseReference/:jiraTicket', async (req, res) => {
     try {
         const { jiraTicket } = req.params;
-        const jiraRequestHeaders = {
-            Authorization: `Basic ${Buffer.from(`${process.env.JIRA_EMAIL}:${process.env.JIRA_API_TOKEN}`).toString("base64")}`
-        };
-
-        const result = await axios.get(`https://${process.env.DOMAIN}.atlassian.net/rest/api/2/issue/${jiraTicket}?fields=status`, {
+        
+        const result = await axios.get(`${siteUrl}/rest/api/2/issue/${jiraTicket}?fields=status`, {
         headers: jiraRequestHeaders
         });
     
@@ -20,8 +30,28 @@ app.get('/case/:caseReference/:jiraTicket', async (req, res) => {
     } catch (error) {
       res.status(400).send(error)
     }
+});
 
-})
+app.post('/transactions/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { jiraTicket } = req.body;
+        const body = {
+            transition: id
+        }
+
+        const result = await axios.post(`${siteUrl}/rest/api/2/issue/${jiraTicket}/transitions`,
+        body,
+        {
+            headers: jiraRequestHeaders
+        });
+    
+      res.status(200).send(result.data)
+    } catch (error) {
+      res.status(400).send(error)
+    }
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
